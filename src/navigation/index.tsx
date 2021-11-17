1819ff087d23522ddd3c87befa600af86f5034d6
DanilovG103 <danilov2706@gmail.com>
2021-11-17 11:38:56 +0400

    feat: styles for tab bar

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,14 +2,49 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Map } from 'src/components/Map';
 import { Settings } from 'src/components/Settings';
+import Icon from 'react-native-vector-icons/Feather';
 
 const Tab = createBottomTabNavigator();
 
 export const Navigation = () => {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Map" component={Map} />
-      <Tab.Screen name="Settings" component={Settings} />
+      <Tab.Screen
+        name="Map"
+        component={Map}
+        options={{
+          tabBarLabel: 'Map',
+          tabBarLabelStyle: {
+            color: '#000',
+            fontFamily: 'Nunito-Regular',
+          },
+          tabBarIcon: ({ focused }) => (
+            <Icon
+              name="map-pin"
+              color={focused ? '#ff0a0a' : '#000'}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarLabel: 'Settings',
+          tabBarLabelStyle: {
+            fontFamily: 'Nunito-Regular',
+            color: '#000',
+          },
+          tabBarIcon: ({ focused }) => (
+            <Icon
+              name="settings"
+              size={24}
+              color={focused ? '#ff0a0a' : '#000'}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
