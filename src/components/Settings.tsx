bff69bc3b16cfc218f124a030232391a7f116cd7
DanilovG103 <danilov2706@gmail.com>
2021-11-17 21:58:58 +0400

    feat: styles for settings

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,41 +1,68 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
 import { useSettings } from 'src/context/Settings';
+import { colors } from 'src/theme/colors';
 import styled from 'styled-components/native';
 
 const Container = styled.View`
   flex: 1;
-  align-items: center;
+  padding: 25px;
 `;
 
 const SettingTitle = styled.Text`
-  color: #000;
+  color: ${colors.dark};
+  font-size: 19px;
+  font-family: 'Nunito-Bold';
+  align-self: center;
 `;
 
 const Pressable = styled.TouchableOpacity`
-  margin: 10px 0;
+  margin: 12px 0;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const SettingDescr = styled.Text`
-  color: #000;
+  color: ${colors.gray[0]};
+  font-size: 15px;
+  font-family: 'Nunito-SemiBold';
+`;
+
+const Circle = styled.View<{ selected: boolean }>`
+  width: 20px;
+  height: 20px;
+  border-radius: 10px;
+  border: 1px solid ${colors.gray[1]};
+  background-color: ${props => (props.selected ? colors.pink : 'transparent')};
 `;
 
 export const Settings = () => {
-  const { setMapType } = useSettings();
+  const { mapType, setMapType } = useSettings();
+
+  const isStandard = mapType === 'standard';
+  const isHybrid = mapType === 'hybrid';
+  const isSatellite = mapType === 'satellite';
+  const isTerrain = mapType === 'terrain';
+
   return (
     <Container>
       <SettingTitle>Map type</SettingTitle>
       <Pressable onPress={() => setMapType('standard')}>
         <SettingDescr>Standard</SettingDescr>
+        <Circle selected={isStandard} />
       </Pressable>
       <Pressable onPress={() => setMapType('hybrid')}>
         <SettingDescr>Hybrid</SettingDescr>
+        <Circle selected={isHybrid} />
       </Pressable>
       <Pressable onPress={() => setMapType('satellite')}>
         <SettingDescr>Satellite</SettingDescr>
+        <Circle selected={isSatellite} />
       </Pressable>
       <Pressable onPress={() => setMapType('terrain')}>
         <SettingDescr>Terrain</SettingDescr>
+        <Circle selected={isTerrain} />
       </Pressable>
     </Container>
   );
---
+++ src/theme/colors.ts
@@ -0,0 +1,5 @@
+export const colors = {
+  dark: '#212526',
+  gray: ['#465B6B', '#c5c4c4'],
+  pink: '#ff9da2',
+};
