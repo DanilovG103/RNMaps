cee0eb3ae9537fd5f8916cf8381cdfb9fc9b4109
DanilovG103 <danilov2706@gmail.com>
2021-11-18 10:19:08 +0400

    fix: imports

--- src/context/MapStyles.tsx
+++ src/context/MapStyles.tsx
@@ -1,6 +1,5 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { createContext, ReactNode, useContext } from 'react';
-import { aubergine, dark, night, retro, silver } from 'src/theme/maps';
 
 const initialState = {
   mapStyle: [],
